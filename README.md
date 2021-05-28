258c979faeb0157032911ceae8193953d41985d9
Maria Nikiforova <nikiforovamary@gmail.com>
2021-05-28 21:49:53 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/m2yrgcmqkraf1ljp?svg=true)](https://ci.appveyor.com/project/nikiforovamaria/ahj-animation)
+
+**Deployment: https://nikiforovamaria.github.io/ahj-animation/**
---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import WidgetCollapsible from './widgetCollapsible';
+import WidgetChat from './widgetChat';
+
+const widgetCollapsible = new WidgetCollapsible();
+widgetCollapsible.init();
+
+const widgetChat = new WidgetChat();
+widgetChat.init();
